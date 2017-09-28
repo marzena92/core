d9493e581c22a35269ae585d67d1b6df8ab94807
Grzegorz Patynek <grzegorz.patynek@goeuropa.eu>
2017-09-28 15:44:01 +0200

    GlobalTeamAvlModule ready

--- transitime/src/main/java/org/transitime/avl/GlobalTeamAvlModule.java
+++ transitime/src/main/java/org/transitime/avl/GlobalTeamAvlModule.java
@@ -15,6 +15,28 @@ import java.net.URL;
 import java.net.URLConnection;
 import org.json.JSONTokener;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.HTTPProxyData;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.select.Elements;
+
 public class GlobalTeamAvlModule extends PollUrlAvlModule {
 
 	private static String avlURL="http://jsonplaceholder.typicode.com/posts/1";
@@ -32,99 +54,111 @@ public class GlobalTeamAvlModule extends PollUrlAvlModule {
                 return avlURL;
         }
 
-	/*private JSONArray getJson(String url){
-	};
-	private ArrayList<Position> getPositions(JSONArray json){
-	}
-	private ArrayList<Device> getDevices(JSONArray json){
-	}
-	private Credentials getCredentials(){
-		String filename = "credentials.txt";
-		  try {
-		   File file = new File(fileName);
-		   Scanner scanner = new Scanner(file);
-		   while (scanner.hasNext()) {
-		    String loginpass = (String)scanner.next();
-		    String[] arr = loginpass.split(",");
-		    Credentials c = new Credentials();
-		    c.login = arr[0];
-		    c.password = arr[1];
-		    return c;
-		   }
-		   scanner.close();
-		  } catch (FileNotFoundException e) {
-		   e.printStackTrace();
-		  }
-		 }
-	}*/
+
+				  public void SSHClient(String serverIp,int serverPort, String command, String usernameString,String password) throws IOException{
+				          System.out.println("inside the ssh function");
+				          try
+				          {
+				              Connection conn = new Connection(serverIp,serverPort);
+				              conn.connect();
+				              boolean isAuthenticated = conn.authenticateWithPassword(usernameString, password);
+				              if (isAuthenticated == false)
+				                  throw new IOException("Authentication failed.");
+				              ch.ethz.ssh2.Session sess = conn.openSession();
+				              sess.execCommand(command);
+				              InputStream stdout = new StreamGobbler(sess.getStdout());
+				              BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+				              System.out.println("the output of the command is");
+				              while (true)
+				              {
+				                  String line = br.readLine();
+				                  if (line == null)
+				                      break;
+				                  System.out.println(line);
+				              }
+				              System.out.println("ExitCode: " + sess.getExitStatus());
+				              sess.close();
+				              conn.close();
+				          }
+				          catch (IOException e)
+				          {
+				              e.printStackTrace(System.err);
+
+				          }
+				      }
         @Override
         protected void processData(InputStream inp) throws Exception {
 
 	ArrayList<AvlReport> avlReports = new ArrayList<AvlReport>();
-        String username = Credentials.username;
-        String password = Credentials.password;
 
-URL url = new URL(this.positionsURL);
-URLConnection uc = url.openConnection();
-String userpass = username + ":" + password;
-String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userpass.getBytes());
-uc.setRequestProperty ("Authorization", basicAuth);
-InputStream in = uc.getInputStream();
+	GlobalTeamAvlModule r = new GlobalTeamAvlModule("777");
+	r.SSHClient(Credentials.ip,Credentials.port,Credentials.path,Credentials.login,Credentials.pass);
+	try {
 
-//      InputStream in = new URL(this.devicesURL).openStream();
+File fXmlFile = new File("gps.xml");
+DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+Document doc = dBuilder.parse(fXmlFile);
 
-        Scanner s = new Scanner(in).useDelimiter("\\A");
-        String JSON_DATA = s.hasNext() ? s.next() : "";
-        System.out.println("JSON DATA: " + JSON_DATA);
+//optional, but recommended
+//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+doc.getDocumentElement().normalize();
 
-//      JSONObject obj = new JSONObject(JSON_DATA);
-                                        JSONArray array = (JSONArray) new JSONTokener(JSON_DATA).nextValue();
-                for (int i=0; i<array.length(); ++i) {
-                                                JSONObject entry = array.getJSONObject(i);
-                                                String deviceId = String.valueOf(entry.getInt("deviceId"));
-                                                String vehicleId = String.valueOf(entry.getInt("id"));
+System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+NodeList nList = doc.getElementsByTagName("LocationRecord");
+
+System.out.println("----------------------------");
+
+for (int temp = 0; temp < nList.getLength(); temp++) {
+
+	Node nNode = nList.item(temp);
+
+	System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+	if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+		Element eElement = (Element) nNode;
+
+		System.out.println("Lat : " + eElement.getElementsByTagName("Latitude").item(0).getTextContent());
+		System.out.println("Long : " + eElement.getElementsByTagName("Longtitude").item(0).getTextContent());
+		System.out.println("TimeOfRecord : " + eElement.getElementsByTagName("TimeOfRecord").item(0).getTextContent());
+		System.out.println("Current orient : " + eElement.getElementsByTagName("CurrentOrientation").item(0).getTextContent());
+		System.out.println("Vehicle ID: " + eElement.getElementsByTagName("VehicleId").item(0).getTextContent());
+
+		String vehicleId = eElement.getElementsByTagName("VehicleId").item(0).getTextContent();
 
 
-                                                Double latitude = entry.getDouble("latitude");
-                                                Double longitude = entry.getDouble("longitude");
-float heading=Float.NaN;
-float speed = (float)entry.getDouble("speed");
-                                                String time = entry.getString("deviceTime");
+		Double latitude = Double.parseDouble(eElement.getElementsByTagName("Latitude").item(0).getTextContent());
+		Double longitude = Double.parseDouble(eElement.getElementsByTagName("Longtitude").item(0).getTextContent());
+		float heading=Float.NaN;
+		float speed = Float.NaN;
+		String time = eElement.getElementsByTagName("TimeOfRecord").item(0).getTextContent();
 
-                                                        //2016-09-07 17:02:48
-						SimpleDateFormat dateformater=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+						//2016-09-07 17:02:48
+		SimpleDateFormat dateformater=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
-                                                        Date timestamp=dateformater.parse(time.replace( "T" , " " ));
-Calendar cal = new GregorianCalendar();
-    cal.setTime(timestamp);
-    cal.add(Calendar.HOUR_OF_DAY, 2);
-//System.out.println("data:"+vehicleId+ timestamp.getTime()+ latitude+longitude+ heading+ speed);
+						Date timestamp=dateformater.parse(time.replace( "T" , " " ));
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(timestamp);
+		cal.add(Calendar.HOUR_OF_DAY, 2);
+		//System.out.println("data:"+vehicleId+ timestamp.getTime()+ latitude+longitude+ heading+ speed);
 
-						if ( ( deviceId.equals("10") == true ) || ( deviceId.equals("11") == true ) || ( deviceId.equals("12") == true ) || ( deviceId.equals("4") == true ) ){
-					AvlReport avlReport = null;
-						if ( deviceId.equals("10") == true )
-	                                                avlReport =
-                                                               new AvlReport("swarzedz4009", cal.getTime().getTime(), latitude,
-                                                                                longitude, heading, speed, "swarzedz4009");
-						if ( deviceId.equals("11") == true )
-	                                                avlReport =
-                                                               new AvlReport("swarzedz4006", cal.getTime().getTime(), latitude,
-                                                                                longitude, heading, speed, "swarzedz4006");
-						if ( deviceId.equals("12") == true )
-	                                                avlReport =
-                                                               new AvlReport("swarzedz4007", cal.getTime().getTime(), latitude,
-                                                                                longitude, heading, speed, "swarzedz4007");
-						if ( deviceId.equals("4") == true )
-																									avlReport =
-																															 new AvlReport("nissan", cal.getTime().getTime(), latitude,
-																																								longitude, heading, speed, "nissan");
-						//avlReport.setAssignment("497-1",AvlReport.AssignmentType.ROUTE_ID);
-	                                        processAvlReport(avlReport);
-						avlReports.add(avlReport);
+			AvlReport avlReport =
+									 new AvlReport("GTavltest", cal.getTime().getTime(), latitude,
+																		longitude, heading, speed, "GTavltest");
+			processAvlReport(avlReport);
+			avlReports.add(avlReport);
+	}
 }
-                                                        }
+	} catch (Exception e) {
+e.printStackTrace();
+	}
+
+
 return;
-        }
+}
+
         /**
          * Just for debugging
          */
