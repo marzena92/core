0a3455573f331c7bcaee65202742d42ff04fb768
Grzegorz Patynek <grzegorz.patynek@goeuropa.eu>
2017-10-02 15:20:54 +0200

    docker cp command

--- transitime/src/main/java/org/transitime/avl/GlobalTeamAvlModule.java
+++ transitime/src/main/java/org/transitime/avl/GlobalTeamAvlModule.java
@@ -37,6 +37,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
 import org.jsoup.select.Elements;
 
+import java.lang.*;
+
 public class GlobalTeamAvlModule extends PollUrlAvlModule {
 
 	private static String avlURL="http://jsonplaceholder.typicode.com/posts/1";
@@ -54,6 +56,42 @@ public class GlobalTeamAvlModule extends PollUrlAvlModule {
                 return avlURL;
         }
 
+				/**
+				 * Execute a bash command. We can handle complex bash commands including
+				 * multiple executions (; | && ||), quotes, expansions ($), escapes (\), e.g.:
+				 *     "cd /abc/def; mv ghi 'older ghi '$(whoami)"
+				 * @param command
+				 * @return true if bash got started, but your command may have failed.
+				 */
+				public static boolean executeBashCommand(String command) {
+				    boolean success = false;
+				    System.out.println("Executing BASH command:\n   " + command);
+				    Runtime r = Runtime.getRuntime();
+				    // Use bash -c so we can handle things like multi commands separated by ; and
+				    // things like quotes, $, |, and \. My tests show that command comes as
+				    // one argument to bash, so we do not need to quote it to make it one thing.
+				    // Also, exec may object if it does not have an executable file as the first thing,
+				    // so having bash here makes it happy provided bash is installed and in path.
+				    String[] commands = {"bash", "-c", command};
+				    try {
+				        Process p = r.exec(commands);
+
+				        p.waitFor();
+				        BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				        String line = "";
+
+				        while ((line = b.readLine()) != null) {
+				            System.out.println(line);
+				        }
+
+				        b.close();
+				        success = true;
+				    } catch (Exception e) {
+				        System.err.println("Failed to execute bash with command: " + command);
+				        e.printStackTrace();
+				    }
+				    return success;
+				}
 
 				  public void SSHClient(String serverIp,int serverPort, String command, String usernameString,String password) throws IOException{
 				          System.out.println("inside the ssh function");
@@ -93,6 +131,13 @@ public class GlobalTeamAvlModule extends PollUrlAvlModule {
 
 	GlobalTeamAvlModule r = new GlobalTeamAvlModule("777");
 	r.SSHClient(Credentials.ip,Credentials.port,Credentials.path,Credentials.login,Credentials.pass);
+
+	try {
+            Thread.sleep(3000);
+						r.executeBashCommand("docker cp /home/onebusaway/gps.xml transitime-server-instance:/gps.xml");
+         } catch (Exception e) {
+            System.out.println(e);
+         }
 	try {
 
 File fXmlFile = new File("gps.xml");
